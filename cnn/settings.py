41463e6e34092093f7852d38ac04f60198f5c368
Ishank Chopra <49484210+ishank1234@users.noreply.github.com>
2020-07-05 11:42:49 +0530

    Update settings.py

--- cnn/settings.py
+++ cnn/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
